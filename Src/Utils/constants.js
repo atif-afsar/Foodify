ded338a130e886289441f88d9dcc25c86347d22f
Atif Afsar <atifafsar70@gmail.com>
2025-05-17 11:17:18 +0530

    url fix

--- Src/Utils/constants.js
+++ Src/Utils/constants.js
@@ -1,21 +1,12 @@
 const LOGO_URL = "https://dynamic.brandcrowd.com/preview/logodraft/496d69a7-57b3-4e75-a337-837851e7e3cc/image/large.png"
 export default LOGO_URL
 
-export const CDN_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.1774553&lng=78.0077653&offset=0&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const CDN_URL = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9582084&lng=77.1255055&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.1774553&lng=78.0077653&restaurantId="
+export const MENU_API = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.1774553&lng=78.0077653&restaurantId="
 
 export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/";
 
-// export const LOGO_URL = "https://dynamic.brandcrowd.com/preview/logodraft/496d69a7-57b3-4e75-a337-837851e7e3cc/image/large.png";
 
-// // This goes through your Express proxy, which maps "/api" → "https://www.swiggy.com"
-// export const CDN_URL = "/api/dapi/restaurants/list/v5?lat=27.1774553&lng=78.0077653&offset=0&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
-// // Keep MENU_API proxied too
-// export const MENU_API = "/api/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.1774553&lng=78.0077653&restaurantId=";
-
-// // Image CDN is public and CORS-free, so no need to proxy
-// export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/";
